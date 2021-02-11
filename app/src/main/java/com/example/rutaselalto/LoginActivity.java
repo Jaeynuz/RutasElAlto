b54919e7147f8cdc0a3224543d277f37b94efae7
J_Gabriel_Ll <gabriel.1803@gmail.com>
2021-02-11 17:19:47 -0400

    una actualizacion completa 11/2

--- app/src/main/java/com/example/rutaselalto/LoginActivity.java
+++ app/src/main/java/com/example/rutaselalto/LoginActivity.java
@@ -1,14 +1,68 @@
 package com.example.rutaselalto;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
+    TextInputEditText mTextInputEmail;
+    TextInputEditText mTextInputPassword;
+    Button mButtonLogin;
+
+
+    FirebaseAuth mAuth;
+    DatabaseReference mDataBase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mTextInputEmail=findViewById(R.id.textInputEmail);
+        mTextInputPassword=findViewById(R.id.textInputPassword);
+        mButtonLogin=findViewById(R.id.btnLogin);
+
+        mAuth=FirebaseAuth.getInstance();
+        mDataBase= FirebaseDatabase.getInstance().getReference();
+
+        mButtonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+    }
+
+    private void login() {
+        String email= mTextInputEmail.getText().toString();
+        String password=mTextInputPassword.getText().toString();
+        if (!email.isEmpty() && !password.isEmpty()){
+            if (password.length() >= 6){
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this, "El inicio de sesion se realizo exitosamente", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(LoginActivity.this, "La contraseña es incorrecta", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+        }
     }
 }
--- app/src/main/java/com/example/rutaselalto/SelectOptionAuthActivity.java
+++ app/src/main/java/com/example/rutaselalto/SelectOptionAuthActivity.java
@@ -1,14 +1,43 @@
 package com.example.rutaselalto;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SelectOptionAuthActivity extends AppCompatActivity {
+    Toolbar mToolbar;
+    Button mButtonGoToLogin;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_option_auth);
+        mToolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Seleccionar Opcion");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mButtonGoToLogin=findViewById(R.id.btnGoToLogin);
+        mButtonGoToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToLogin();
+
+            }
+        });
     }
+
+    public void goToLogin() {
+        Intent intent= new Intent(SelectOptionAuthActivity.this,LoginActivity.class );
+        startActivity(intent);
+
+    }
+
+
 }
